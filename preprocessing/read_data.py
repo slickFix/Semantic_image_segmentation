4d98028c4f79adba6a79207269db856f949b79da
slickFix <sidd.200894@gmail.com>
2019-05-02 09:31:48 +0530

    completed download_resnet_checkpoint_if_necessary function in read.py

--- preprocessing/read_data.py
+++ preprocessing/read_data.py
@@ -6,3 +6,27 @@ Created on Wed May  1 12:35:44 2019
 @author: siddharth
 """
 
+import os
+import urllib
+import tarfile
+
+def download_resnet_checkpoint_if_necessary(resnet_checkpoints_path,resnet_model_name):
+    
+    if not os.path.exists(resnet_checkpoints_path):
+        
+        # creating the path and downloading the checkpoints
+        os.mkdir(resnet_checkpoints_path)
+        
+        filename = resnet_model_name+'_2017_04_14.tar.gz'
+        
+        url = "http://download.tensorflow.org/models/" + filename
+        fullpath = os.path.join(resnet_checkpoints_path,filename)
+        urllib.request.urlretrieve(url,fullpath)
+        
+        tarfile_ = tarfile.open(fullpath,"r:gz")
+        tarfile_.extractall(path = resnet_checkpoints_path)
+        tarfile_.close()
+        
+        print("Resnet checkpoints for model ",resnet_model_name," downloaded successfully.")
+    else:
+        print("Resnet checkpoints already exits")
