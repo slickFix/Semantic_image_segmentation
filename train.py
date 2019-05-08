6c856b61ec3e72238fb1d48b8f27055f3d0d2778
slickFix <sidd.200894@gmail.com>
2019-05-08 16:17:42 +0530

    completed validation dataset creation in train.py

--- train.py
+++ train.py
@@ -86,12 +86,18 @@ resnet_checkpoints_path = './resnet/checkpoints/'
 
 training_filenames = [os.path.join(TRAIN_DATASET_DIR,TRAIN_FILE)]
 training_dataset = tf.data.TFRecordDataset(training_filenames)
-training_dataset = training_dataset.map(tf_record_parser)
+training_dataset = training_dataset.map(tf_record_parser) # Parse the record in tensors
 training_dataset = training_dataset.map(rescale_image_and_annotation_by_factor)
 training_dataset = training_dataset.map(distort_randomly_image_color)
 training_dataset = training_dataset.map(lambda image,annotation,image_shape:scale_image_with_crop_padding(image,annotation,image_shape,crop_size))
-training_dataset = training_dataset.map(random_flip_image_and_annotation) # Parse the record in tensors
+training_dataset = training_dataset.map(random_flip_image_and_annotation) 
 training_dataset = training_dataset.repeat()  # no of epochs
 training_dataset = training_dataset.shuffle(buffer_size=500)
 training_dataset = training_dataset.batch(args.batch_size)
 
+validation_filenames = [os.path.join(TRAIN_DATASET_DIR,VALIDATION_FILE)]
+validation_dataset = tf.data.TFRecordDataset(validation_filenames)
+validation_dataset = validation_dataset.map(tf_record_parser) # Parse the record in tensors
+validation_dataset = validation_dataset.map(lambda image,annotation,image_shape:scale_image_with_crop_padding(image,annotation,image_shape,crop_size))
+validation_dataset = validation_dataset.shuffle(buffer_size = 100)
+validation_dataset = validation_dataset.batch(args.batch_size)
