a2a702a80c7818ad7e93fabd82f1389cba8713db
slickFix <sidd.200894@gmail.com>
2019-05-24 17:52:05 +0530

    implemented atrous_spatial_pyramid_pooling function in network.py

--- network.py
+++ network.py
@@ -16,6 +16,42 @@ _R_MEAN = 123.68
 _G_MEAN = 116.78
 _B_MEAN = 103.94
 
+
+@slim.add_arg_scope
+def atrous_spatial_pyramid_pooling(net,scope,depth=256,reuse=None):
+    
+    """
+    ASPP consists of (a) one 1×1 convolution and three 3×3 convolutions with rates = (6, 12, 18) when output stride = 16
+    (all with 256 filters and batch normalization), and (b) the image-level features as described in https://arxiv.org/abs/1706.05587
+    :param net: tensor of shape [BATCH_SIZE, WIDTH, HEIGHT, DEPTH]
+    :param scope: scope name of the aspp layer
+    :return: network layer with aspp applyed to it.
+    """
+    
+    with tf.variable_scope(scope,reuse=reuse):
+        
+        feature_map_size = tf.shape(net)
+        
+        # apply global average pooling 
+        image_level_features = tf.reduce_mean(net,[1,2],name='image_level_global_pool',keepdims = True)
+        
+        image_level_features = slim.conv2d(image_level_features,depth,[1,1],scope = 'image_level_conv_1x1',activation_fn=None)
+        image_level_features = tf.image.resize_bilinear(image_level_features,(feature_map_size[1],feature_map_size[2]))
+        
+        at_pool1x1 = slim.conv2d(net,depth,[1,1],scope='conv_1x1_0',activation_fn=None)
+        
+        at_pool3x3_1 = slim.conv2d(net,depth,[3,3],scope = 'conv_3x3_1',rate=6,activation_fn=None)
+        
+        at_pool3x3_2 = slim.conv2d(net,depth,[3,3],scope = 'conv_3x3_2',rate=12,activation_fn=None)
+        
+        at_pool3x3_3 = slim.conv2d(net,depth,[3,3],scope = 'conv_3x3_3',rate=18,activation_fn=None)
+        
+        net = tf.concat((image_level_features,at_pool1x1,at_pool3x3_1,at_pool3x3_2,at_pool3x3_3),axis=3,name='concat')
+        
+        net = slim.conv2d(net,depth,[1,1],scope='conv_1x1_output',activation_fn=None)
+        
+        return net
+
 def deeplab_v3(inputs,args,is_training,reuse):
     
     
