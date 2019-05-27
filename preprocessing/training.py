4447cc5340f2d3e6fa8e24461a6867e3cd63d5a1
slickFix <sidd.200894@gmail.com>
2019-05-27 18:16:44 +0530

    initial commit preprocessing/training.py

---
+++ preprocessing/training.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 27 18:13:23 2019
+
+@author: siddharth
+"""
+
+import tensorflow as tf
+from tensorflow.python.ops import control_flow_ops
+slim = tf.contrib.slim
+
