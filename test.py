ad57c713095af2eeb8f778f0574ca69edb1e6233
slickFix <sidd.200894@gmail.com>
2019-05-29 23:52:58 +0530

    implemented argument parser and train file arguments in test.py

--- test.py
+++ test.py
@@ -21,3 +21,37 @@ from preprocessing.read_data import tf_record_parser,scale_image_with_crop_paddi
 from preprocessing import training_util
 
 from metrics import *
+
+plt.interactive(False)
+
+
+# =============================================================================
+# # defining argument parser
+# =============================================================================
+parser = argparse.ArgumentParser()
+
+envrag = parser.add_argument_group("Eval params")
+envrag.add_argument("--model_id",default=2019,type=int,help="Model id name  to be loaded ")
+input_args = parser.parse_args()
+
+model_name = str(input_args.model_id)
+
+
+# =============================================================================
+# # restoring train file arguments 
+# =============================================================================
+
+log_folder = './tboard_logs'
+
+with open(log_folder+'/'+model_name+'/train/data.json','r') as fp:
+    args = json.load(fp)
+
+class Dotdict(dict):
+     """dot.notation access to dictionary attributes"""
+     __getattr__ = dict.get
+     __setattr__ = dict.__setitem__
+     __delattr__ = dict.__delitem__
+
+args = Dotdict(args)    
+
+
