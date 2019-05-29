073897b04978262aa48f74d6b638bb9dcd80baad
slickFix <sidd.200894@gmail.com>
2019-05-29 17:45:04 +0530

    completed imports in test.py

--- test.py
+++ test.py
@@ -6,3 +6,18 @@ Created on Wed May 29 17:35:13 2019
 @author: siddharth
 """
 
+# importing libraries
+
+import tensorflow as tf
+print(" Tf version : ", tf.__version__)
+import numpy as np
+from matplotlib import pyplot as plt
+import neural_network
+slim = tf.contrib.slim
+import os
+import argparse
+import json
+from preprocessing.read_data import tf_record_parser,scale_image_with_crop_padding
+from preprocessing import training_util
+
+from metrics import *
