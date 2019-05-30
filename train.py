16bb08f67b481ad9115ec4e18a72d8049ba64462
slickFix <sidd.200894@gmail.com>
2019-05-30 11:46:53 +0530

    adding training_dataset.repeat() comment

--- train.py
+++ train.py
@@ -103,7 +103,7 @@ training_dataset = training_dataset.map(rescale_image_and_annotation_by_factor)
 training_dataset = training_dataset.map(distort_randomly_image_color)
 training_dataset = training_dataset.map(lambda image,annotation,image_shape:scale_image_with_crop_padding(image,annotation,image_shape,crop_size))
 training_dataset = training_dataset.map(random_flip_image_and_annotation) 
-training_dataset = training_dataset.repeat()  # no of epochs
+training_dataset = training_dataset.repeat()  # no of epochs (no values means inf time repeat)
 training_dataset = training_dataset.shuffle(buffer_size=500)
 training_dataset = training_dataset.batch(args.batch_size)
 
